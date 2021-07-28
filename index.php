7081a34d96cf9dceee28a3ca3aba912dd95de092
Ahsan <asn.cs21@gmail.com>
2021-07-28 02:32:23 -0700

    Make Footer Reuseable in components

--- index.php
+++ index.php
@@ -68,22 +68,9 @@
     </section>
 
     <!-- ------ Footer part --------------------------  -->
-
-    <footer> 
-      <div class="container">
-        <div class="footer-styling">
-          <h1>ASN Discussion</h1>
-          <ul class="footer-nav">
-            <li><a href="#" target="_blank">About Me</a></li>
-            <li><a href="#" target="_blank">LinkedIn</a></li>
-            <li><a href="#" target="_blank">GitHub</a></li>
-            <li><a href="#" target="_blank">Instagram</a></li>
-          </ul>
-          <div class="row"><p class="copyrights">Copy right &copy;By Rana Ahsan Ansar. All rights reserved.</p></div>
-        </div>
-      </div>
-    </footer>
-
+    <?php
+    require 'components/footer.php';
+    ?>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
